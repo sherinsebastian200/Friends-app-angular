982e7753d8a5d047b8bc971b9143e82ea89c03f4
sherin200 <sherinsebastian21>
2022-11-25 18:39:39 +0530

    msg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AddFriendComponent } from './add-friend/add-friend.component';
 import { ViewFriendComponent } from './view-friend/view-friend.component';
 import { SearchFriendComponent } from './search-friend/search-friend.component';
 import { RouterModule, Routes } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 const myRoute:Routes=[
   {
     path:"",
@@ -27,7 +28,8 @@ const myRoute:Routes=[
     AppComponent,
     AddFriendComponent,
     ViewFriendComponent,
-    SearchFriendComponent
+    SearchFriendComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
